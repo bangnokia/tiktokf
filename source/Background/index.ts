baee88bfb30b6e7845a250503a274c679f0d5907
bangnokia <bangnokia@gmail.com>
2022-05-29 22:45:32 +0700

    work in progress

--- source/Background/index.ts
+++ source/Background/index.ts
@@ -4,5 +4,5 @@ import { browser } from "webextension-polyfill-ts";
 console.log('background script loaded')
 
 browser.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-  console.log('chanaaaaaaaaaaaaaaa', tabId, changeInfo, tab);
+  // what should we do here
 });
