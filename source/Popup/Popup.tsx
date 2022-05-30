6f342a79646375797d329a615d0a9b2a4db2edd1
bangnokia <bangnokia@gmail.com>
2022-05-30 22:10:31 +0700

    update instruction

--- source/Popup/Popup.tsx
+++ source/Popup/Popup.tsx
@@ -3,16 +3,16 @@ import * as React from 'react';
 import './styles.scss';
 const Popup: React.FC = () => {
   return (
-    <section id="popup">
+    <section id="popup" style={{ fontSize: '2rem' }}>
       <h1 style={{ fontWeight: 'bold' }}>Keyboard shortcuts</h1>
       <ul style={{
         display: 'flex',
         flexDirection: 'column',
         color: 'sky'
       }}>
-        <li>Press <kbd style={{ fontWeight: 'bold' }}>F</kbd> to enter full screen of current video</li>
-        <li>Press <kbd style={{ fontWeight: 'bold' }}>ESC</kbd> to exit full screen of current video</li>
-        <li>Press <kbd style={{ fontWeight: 'bold' }}>T</kbd> to toggle sidebar</li>
+        <li>Press <kbd style={{ fontWeight: 'bold' }}>F</kbd> to enter full screen of the current video</li>
+        <li>Press <kbd style={{ fontWeight: 'bold' }}>ESC</kbd> to exit full screen of the current video</li>
+        <li>Press <kbd style={{ fontWeight: 'bold' }}>T</kbd> to toggle the sidebar</li>
       </ul>
     </section>
   );
