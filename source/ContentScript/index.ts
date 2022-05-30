9e18b526ac5b4c70463ca529f1fac86486020fa4
bangnokia <bangnokia@gmail.com>
2022-05-30 11:17:38 +0700

    press F to go fullscreen

--- source/ContentScript/index.ts
+++ source/ContentScript/index.ts
@@ -1,7 +1,14 @@
 import { injectCss } from '../utils'
+import hotkeys from 'hotkeys-js';
 
 injectCss();
 
+hotkeys('f', () => {
+  console.log('F pressed');
+  const activeSlider = document.querySelector('.swiper-slide-active');
+  const expandButton = activeSlider?.querySelector('[class*=\'-DivExpandIconContainer\']');
+  expandButton?.click();
+});
 
 
 
